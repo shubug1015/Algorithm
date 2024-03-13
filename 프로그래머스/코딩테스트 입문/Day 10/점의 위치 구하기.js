ca01c2d0f9362faab6718976e4af2de93d2191f5
Donghyun Lee <shubug@naver.com>
2024-03-13 17:43:43 +0900

    점의 위치 구하기

---
+++ 프로그래머스/코딩테스트 입문/Day 10/점의 위치 구하기.js
@@ -0,0 +1,12 @@
+function solution(dot) {
+    const x = dot[0], y = dot[1];
+    
+    if (x > 0 && y > 0)
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else
+        return 4
+}
