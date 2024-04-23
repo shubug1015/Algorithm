410ba99d5290dbd357a81fd5aa1d41ec021b39af
Donghyun Lee <shubug@naver.com>
2024-04-23 15:42:25 +0900

    완주하지 못한 선수

--- 프로그래머스/고득점 Kit/해시/완주하지 못한 선수.js
+++ 프로그래머스/고득점 Kit/해시/완주하지 못한 선수.js
@@ -1,11 +1,18 @@
-function solution(participantList, completionList) {
-    completionList.forEach((completion) => {
-        participantList.forEach((participant, index) => {
-            if (completion === participant) {
-                participantList.splice(index, 1);
-            }
-        })
-    });
+function solution(participant, completion) {
+    const map = new Map();
 
-    return participantList.join('');
+    for (let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);        
+        map.set(b, (map.get(b) || 0) - 1);        
+    }
+
+    for (let [k, v] of map) {
+        if (v > 0)
+            return k;
+    }
+
+    return 'nothing';
 }
